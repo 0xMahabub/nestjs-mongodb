b362a922b200aa2366cb411f162e77611822e964
0xMahabub <mahabub212002072@gmail.com>
2022-04-21 21:38:06 +0600

    CRUD --tested --OK

---
+++ src/product/product.controller.ts
@@ -0,0 +1,63 @@
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { ProductService } from './product.service';
+
+@Controller('product')
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Post()
+  async addProduct(
+    @Body('title') pTitle: string,
+    @Body('description') pDesc: string,
+    @Body('price') pPrice: number,
+  ) {
+    const generatedId = await this.productService.addProduct(
+      pTitle,
+      pDesc,
+      pPrice,
+    );
+
+    return { id: generatedId };
+  }
+
+  @Get()
+  async getAllProducts() {
+    const products = await this.productService.getProducts();
+    return products;
+  }
+
+  @Get(':id')
+  getProduct(@Param('id') prodId: string) {
+    return this.productService.getSingleProduct(prodId);
+  }
+
+  @Patch(':id')
+  async updateProduct(
+    @Param('id') prodId: string,
+    @Body('title') prodTitle: string,
+    @Body('description') prodDesc: string,
+    @Body('price') prodPrice: number,
+  ) {
+    await this.productService.updateProduct(
+      prodId,
+      prodTitle,
+      prodDesc,
+      prodPrice,
+    );
+    return null;
+  }
+
+  @Delete(':id')
+  async removeProduct(@Param('id') prodId: string) {
+    await this.productService.deleteProduct(prodId);
+    return null;
+  }
+}
---
+++ src/product/product.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductSchema } from './product.model';
+import { ProductService } from './product.service';
+import { ProductController } from './product.controller';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }]),
+  ],
+  providers: [ProductService],
+  controllers: [ProductController],
+})
+export class ProductModule {}
